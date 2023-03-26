d66dd30b0ac89df0b5deb01ac4dddc69464905f0
Ziru Zou <jiyucoding@yahoo.com>
2023-03-26 10:18:02 -0400

    ass solution

--- server/app.py
+++ server/app.py
@@ -8,99 +8,130 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
+    
     def post(self):
-        # Create a new user with the username in signup form
-        username = request.get_json()["username"]
-        password = request.get_json()["password"]
-        image_url = request.get_json()["image_url"]
-        bio = request.get_json()["bio"]
 
-        if username and password:
-            new_user = User(
-                username=username,
-                image_url=image_url,
-                bio=bio
-            )
+        request_json = request.get_json()
 
-            # encrypted password, image URL, and bio.
-            new_user.password_hash = password
-            # new_user.image_url = request.get_json()["image_url"]
-            # new_user.bio = request.get_json()["bio"]
-            # Save a new user to the database 
-            db.session.add(new_user)
+        username = request_json.get('username')
+        password = request_json.get('password')
+        image_url = request_json.get('image_url')
+        bio = request_json.get('bio')
+
+        user = User(
+            username=username,
+            image_url=image_url,
+            bio=bio
+        )
+
+        # the setter will encrypt this
+        user.password_hash = password
+
+        print('first')
+
+        try:
+
+            print('here!')
+
+            db.session.add(user)
             db.session.commit()
 
-            # Save the user's ID in the session object as user_id.
-            session["user_id"] = new_user.id
-            # Return a JSON response, HTTP status code of 201 (Created)
-            return new_user.to_dict(), 201
-        # If the user is not valid: error message + 422
-        return {'error': '422 Unprocessable Entity'}, 422
+            session['user_id'] = user.id
 
+            print(user.to_dict())
+
+            return user.to_dict(), 201
+
+        except IntegrityError:
+
+            print('no, here!')
+            
+            return {'error': '422 Unprocessable Entity'}, 422
 
 class CheckSession(Resource):
+    
     def get(self):
-        # if the user is logged in (if their user_id is in the session object):
-        if session.get("user_id"):
-            # Return a JSON response + an HTTP status code of 200
-            user = User.query.filter_by(id=session["user_id"]).first()
+
+        if session.get('user_id'):
+
+            user = User.query.filter(User.id == session['user_id']).first()
+
             return user.to_dict(), 200
-        # not logged in: Return a JSON response + 401 (Unauthorized).
-        return {"error": {"401: Unauthorized user"}}, 401
+
+        return {'error': '401 Unauthorized'}, 401
 
 class Login(Resource):
+    
     def post(self):
-        username = request.get_json()["username"]
-        password = request.get_json()["password"]
-        user = User.query.filter_by(username=username).first()
-        if user.authenticate(password):
-            session["user_id"] = user.id
-            return user.to_dict(), 200
-        return {"error": "401: Unauthorized"}, 401
 
+        request_json = request.get_json()
+
+        username = request_json.get('username')
+        password = request_json.get('password')
+
+        user = User.query.filter(User.username == username).first()
+
+        if user:
+            if user.authenticate(password):
+
+                session['user_id'] = user.id
+                return user.to_dict(), 200
+
+        return {'error': '401 Unauthorized'}, 401
 
 class Logout(Resource):
+    
     def delete(self):
-        # if the user is logged in 
+        
         if session.get('user_id'):
-            # Remove the user's ID from the session object.
-            session["user_id"] = None
-            # Return an empty response with an HTTP status code of 204 (No Content).
+            
+            session['user_id'] = None
+            
             return {}, 204
-        return {"error": "401: Unauthorized"}, 401
-
+        
+        return {'error': '401 Unauthorized'}, 401
 
 class RecipeIndex(Resource):
-    def get(self):
-        # if the user is logged in (if their user_id is in the session object):
-        # Return a JSON response with an array of all recipes with their title, instructions, and minutes to complete data 
-        # along with a nested user object; and an HTTP status code of 200 (Success).
-        if session.get("user_id"):
-            recipes = Recipe.query.all()
-            recipes_dict = [recipe.to_dict(rules=("users")) for recipe in recipes]
-            return make_response(recipes_dict, 200)
-        return {"error": "401: Unauthorized"}, 401
-    
-    # create new recipes
-    def post(self):
-        if session.get("user_id"):
-            title = request.get_json()["title"]
-            instructions = request.get_json()["instructions"]
-            minutes_to_complete = request.get_json()["minutes_to_complete"]
-            # Save a new recipe to the database if it is valid. along with a nested user object
-            if title and instructions and minutes_to_complete:
-                new_recipe = Recipe(
-                    title=title, instructions=instructions, minutes_to_complete=minutes_to_complete
-                )
-                db.session.add(new_recipe)
-                db.session.commit()
-                session["user_id"] = new_recipe.id
-                return new_recipe.to_dict("users"), 201
-            # If the recipe is not valid
-            return {"error": "422: Unprocessable Entity"}, 422
-        # If the user is not logged in
-        return {"error", "401: Unauthorized"}, 401
 
+    def get(self):
+
+        if session.get('user_id'):
+
+            user = User.query.filter(User.id == session['user_id']).first()
+
+            return [recipe.to_dict() for recipe in user.recipes], 200
+        
+        return {'error': '401 Unauthorized'}, 401
+        
+    def post(self):
+
+        if session.get('user_id'):
+
+            request_json = request.get_json()
+
+            title = request_json['title']
+            instructions = request_json['instructions']
+            minutes_to_complete = request_json['minutes_to_complete']
+
+            try:
+
+                recipe = Recipe(
+                    title=title,
+                    instructions=instructions,
+                    minutes_to_complete=minutes_to_complete,
+                    user_id=session['user_id'],
+                )
+
+                db.session.add(recipe)
+                db.session.commit()
+
+                return recipe.to_dict(), 201
+
+            except IntegrityError:
+
+                return {'error': '422 Unprocessable Entity'}, 422
+
+        return {'error': '401 Unauthorized'}, 401
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
--- server/models.py
+++ server/models.py
@@ -1,73 +1,50 @@
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.orm import validates
-
 
 from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    serialize_rules = ("-recipes.user", "-_password_hash",)
+    serialize_rules = ('-recipes.user', '-_password_hash',)
 
     id = db.Column(db.Integer, primary_key=True)
-    # validate the user's username to ensure that it is present and unique (no two users can have the same username).
     username = db.Column(db.String, unique=True, nullable=False)
     _password_hash = db.Column(db.String)
     image_url = db.Column(db.String)
     bio = db.Column(db.String)
 
-    # have many recipes.
-    recipes = db.relationship("Recipe", backref='user')
+    recipes = db.relationship('Recipe', backref='user')
 
-    # @validates("username")
-    # def validate_username(self, key, username):
-    #     if not username:
-    #         raise ValueError("Must have username.")
-    
-    # ================= incorporate bcrypt to create a secure password. ====================
     @hybrid_property
     def password_hash(self):
-        # Attempts to access the password_hash should be met with an AttributeError.
-        raise AttributeError("Password hashes may not be accessed")
-    
+        raise AttributeError('Password hashes may not be viewed.')
+
     @password_hash.setter
     def password_hash(self, password):
         password_hash = bcrypt.generate_password_hash(
-            password.encode('utf-8')
-        )
+            password.encode('utf-8'))
         self._password_hash = password_hash.decode('utf-8')
-    
+
     def authenticate(self, password):
         return bcrypt.check_password_hash(
-            self._password_hash, password.encode('utf-8')
-        )
-    # =====================================================================================
+            self._password_hash, password.encode('utf-8'))
 
     def __repr__(self):
-        return f'''<User {self.id}: Username: {self.username}>'''
+        return f'<User {self.username}>'
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
-    
+    __table_args__ = (
+        db.CheckConstraint('length(instructions) >= 50'),
+    )
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     instructions = db.Column(db.String, nullable=False)
     minutes_to_complete = db.Column(db.Integer)
 
-    # a recipe belongs to a user.
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-
-    # @validates('title')
-    # def validate_title(self, key, title):
-    #     if not title:
-    #         raise ValueError("title must be present.")
-    
-    @validates("instructions")
-    def validate_instructions(self, key, instructions):
-        if len(instructions) < 50:
-            raise ValueError("instructions must be present and at least 50 characters long.")
-
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
 
     def __repr__(self):
-        return f'''<Recipe {self.id}: Title: {self.title}>'''
+        return f'<Recipe {self.id}: {self.title}>'
